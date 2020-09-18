c8444aa6a352ed468b4aa681bd1cd3bec4841a7a
gotaek <64013443+gotaek@users.noreply.github.com>
2020-09-19 00:30:46 +0900

    momentom 0919

---
+++ greeting.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+//지역 저장소에 저장하는 함수
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+//이벤트가 들어오면 출력하고 저장하는 함수
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+//form을 보여주며 이름을 묻는 함수
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+//form을 보여주지 않으며 출력을 해주는 함수
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `hello ${text}`;
+}
+//지역저장소에서 값을 받아 값이 없으면 이름을 물어보고 값이 있으면 출력하는 함수
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
---
+++ todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+//지역 저장소에 배열을 string으로 바꾸어서 저장
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+//객체를 만들고 배열에 추가한 후 저장
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerHTML = "x";
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+//이벤트가 들어오면 paint
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+//지금 까지 저장되어 있는 배열을 다시 패인트
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
+  }
+}
+
+function init() {
+  loadToDos(); //지금까지 있는 것을 로드
+  toDoForm.addEventListener("submit", handleSubmit); //새롭게 추가
+}
+init();
