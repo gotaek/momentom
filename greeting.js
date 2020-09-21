b0d6a9cf4a332f7c24ad92335e7336a429228626
gotaek <64013443+gotaek@users.noreply.github.com>
2020-09-22 01:16:40 +0900

    momentom 0922

--- greeting.js
+++ greeting.js
@@ -4,10 +4,12 @@ const form = document.querySelector(".js-form"),
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
+
 //지역 저장소에 저장하는 함수
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
+
 //이벤트가 들어오면 출력하고 저장하는 함수
 function handleSubmit(event) {
   event.preventDefault();
@@ -15,17 +17,20 @@ function handleSubmit(event) {
   paintGreeting(currentValue);
   saveName(currentValue);
 }
+
 //form을 보여주며 이름을 묻는 함수
 function askForName() {
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
 }
+
 //form을 보여주지 않으며 출력을 해주는 함수
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `hello ${text}`;
 }
+
 //지역저장소에서 값을 받아 값이 없으면 이름을 물어보고 값이 있으면 출력하는 함수
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 //지역 저장소에 배열을 string으로 바꾸어서 저장
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,6 +27,7 @@ function paintToDo(text) {
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerHTML = "x";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
